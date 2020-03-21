9c95848dd19845582e6d0b6fab28be29060128dd
huggingpixels <eszter1914@gmail.com>
2020-03-21 08:23:04 +0100

    floating duck in resume section

--- src/Resume.svelte
+++ src/Resume.svelte
@@ -1,1 +1,38 @@
-<div id="resume">Here comes the resume section</div>
+<script>
+    import Duck from "./assets/Duck.svelte";
+</script>
+
+<style>
+    #pond {
+        height: 200px;
+    }
+
+    #duck {
+	    transform: translate(0px, 15px);
+        animation: float 10s ease infinite;
+        position: absolute;
+    }
+  
+    @keyframes float {
+	    0% {
+	    	transform: translate(-50px, 15px);
+        }
+        25% {
+	    	transform: translate(200px, 0px) scale(0.8);
+        }
+        50% {
+	    	transform: translate(450px, 15px);
+	    }
+	    75% {
+	    	transform: translate(700px, 0px) scale(0.8);
+        }
+	    100% {
+		    transform: translate(950px, 15px);
+	    }
+    }
+</style>
+
+<h3 id="resume">Catch a duck to check my linkedin page</h3>
+<div id="pond">
+    <span id="duck"><Duck /></span>
+</div>
